1499d8ff64f89ad147f80a295db9b2a3898330d3
adiabil <User@almayos-mekonen-MacBook-Air.local>
2024-08-01 10:57:52 +0300

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export",
-// };
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-// export default nextConfig;
+export default nextConfig;
