c71849482e2ae23778ff2d933bd1914a87f123a0
adiabil <User@almayos-mekonen-MacBook-Air.local>
2024-08-01 11:44:55 +0300

    change

--- app/(content)/athletes/[slug]/image/page.jsx
+++ app/(content)/athletes/[slug]/image/page.jsx
@@ -1,10 +1,16 @@
 import { notFound } from "next/navigation";
-import { getFighterBySlug } from "@/lib/fightersService";
+import { getFighterBySlug, getAllFighters } from "@/lib/fightersService";
 import classes from "./page.module.css";
+
+export const generateStaticParams = async () => {
+  const fighters = await getAllFighters();
+  return fighters.map((fighter) => ({
+    slug: fighter.slug,
+  }));
+};
 
 const ImagePage = async ({ params }) => {
   const newsItemSlug = params.slug;
-
   const imageItem = await getFighterBySlug(newsItemSlug);
 
   if (!imageItem) {
